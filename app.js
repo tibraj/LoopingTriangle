1cb200780148cf1373c6c1ec477c684d8a0e3396
tibraj <tibraj94@gmail.com>
2021-01-01 16:59:18 -0500

    loop triangle method

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-document.getElementById('loop').addEventListener('click', loopTriangle);
+document.getElementById('loop').addEventListener('click', loopTriangle());
+
+function loopTriangle() {
+    for(let line="#"; line.length < 8; line+= "#") {
+    console.log(line);
+    };
+}
